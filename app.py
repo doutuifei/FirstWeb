f243409cd02de5aa957f664134022b60c8f05b7b
muzi <727784430@qq.com>
2018-08-22 16:34:01 +0800

    添加404

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # -*-coding:utf-8-*-
-from flask import Flask
+from flask import Flask, make_response
 from flask import request
 from flask import Response
 from flask import send_file
@@ -7,14 +7,27 @@ from flask import render_template
 
 app = Flask(__name__, static_url_path='')
 
+
 # css静态资源必须放到static目录下
 @app.route('/404', methods=['GET', 'POST'])
 def handle404():
     return app.send_static_file('html/404.html')
 
-@app.route('/')
+
+@app.errorhandler(404)
+def not_found(error):
+    return app.send_static_file('html/404.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return "hello!"
+    if request.method == 'GET':
+        print(request.method, request.args)
+
+    elif request.method == 'POST':
+        print(request.method, request.form)
+    return "hello 典!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10708)
